3c3632b84c8a313c2489f1e0a0e7c0f4e350d047
David Wu <david10608@gmail.com>
2017-12-11 15:28:09 +0100

    Test driver

--- src/LK/CustomList/Test.java
+++ src/LK/CustomList/Test.java
@@ -2,6 +2,10 @@ package LK.CustomList;
 
 public class Test {
     public static void main(String... args) {
-        
+        CustomList<Integer> list = new CustomList<>();
+        list.add(8);
+        list.add(5);
+        System.out.println(list);
+        System.out.println(list.get(1));
     }
 }
